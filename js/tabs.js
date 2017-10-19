61e3c248585b704bcde9cb0095ac670a9323e318
tugenhua <tugenhua@tugenhuadeMacBook-Pro.local>
2017-10-20 01:56:01 +0800

    update

--- js/tabs.js
+++ js/tabs.js
@@ -4,6 +4,27 @@
 */
 function AddTabs(cfg) {
 
+  /*
+    tab点击的某一项，支持传 tabItem 配置项，如果有该项 代码内部处理点击事件
+    // 比如如下html代码：
+    <a href="#" class="easyui-linkbutton J_Tabs" data-title='2015测试' data-value='1'>2015</a>
+    <a href="#" class="easyui-linkbutton J_Tabs" data-title='2016测试' data-value='2'>2016</a>
+    <a href="#" class="easyui-linkbutton J_Tabs" data-title='2017测试' data-value='3'>2017</a>
+    注意： 内部点击 需要传入 data-title 和 data-value 
+    1. data-title 是指tab项的标题。
+    2. data-value="1" 是指tab项内容的索引 比如 tabContent1；那么在页面上会有 id为 tabContent1，那么我会根据该id获取所有的html
+    动态的渲染内容。
+   */ 
+
+  // tab需要点击的某一项
+  this.tabItem = cfg.tabItem || null;
+
+  // tab 某一项的标题
+  this.title = cfg.title || '';
+
+  // tab某一项需要的显示的内容
+  this.content = cfg.content || '';
+
   // tab项的容器
   this.container = cfg.container;
 
@@ -14,36 +35,35 @@ function AddTabs(cfg) {
     throw new Error('tab项的容器不能为空');
     return;
   }
-  // 输入框的值
-  var reg = /^\d+$/g;
-  if (!reg.test(cfg.value)) {
-    // 如果不是数字的话, 直接返回
-    throw new Error('值需要是纯数字');
-    return;
+  if (this.tabItem) {
+    this.bindEnv();
+  } else {
+    if (!cfg.title) {
+      throw new Error('tab项的标题不能为空');
+      return;
+    }
+    if (!cfg.content) {
+      throw new Error('tab项的内容不能为空');
+      return;
+    }
+    var container = $(this.container);
+    var title = this.title;
+    var content = this.content;
+    this.addTabs(title, content, container);
   }
-  this.value = cfg.value - 0;
-
-  // 内容容器的前缀
-  this.prefixContent = cfg.prefixContent;
-
-  this.addTabs();
-  return this;
 }
-AddTabs.prototype.constructor = AddTabs;
-AddTabs.prototype.addTabs = function() {
-  var self = this;
-  var values = this.value;
-  var container = this.container;
-  $(container).html('');
-  var $tabs = $('<div class="easyui-tabs" style="width:400px;height:250px;"></div>');
-  $(container).append($tabs);
-  if (values >= 0) {
-    for (var i = 0; i < values; i++) {
-      var title = (i+1);
-      var content = $('#' + this.prefixContent + title).html();
-      $($tabs).tabs({
+
+AddTabs.prototype = {
+  addTabs: function(title, content, container) {
+    var self = this;
+    if ($(container).tabs('exists', title)) {
+      $(container).tabs('select', title);
+    } else {
+      $(container).removeClass('hidden');
+      $(container).tabs({
         onAdd: function() {
           var $this = $(this);
+          console.log($this)
           if($this.find('.tabs-wrap .arrow_icon').length < 1) {
             $this.find('.tabs-wrap').append('<i class="arrow_icon"></i>');
           }
@@ -51,49 +71,43 @@ AddTabs.prototype.addTabs = function() {
           self.panelUpAndDown($this);
         },
         onClose: function(title, index){
-          if ($($tabs).tabs('tabs').length === 0) {
-            setTimeout(function(){
-              $(container).html('');
-            }, 50);
+          if ($(container).tabs('tabs').length === 0) {
+            $(container).addClass('hidden');
           }
-          var obj = {
-            $this: self,
-            title: title,
-            index: index,
-            len: $($tabs).tabs('tabs').length
-          };
-          setTimeout(function(){
-            self.closedItemCallBack && $.isFunction(self.closedItemCallBack) && self.closedItemCallBack(obj);
-          }, 40);
+          self.closedItemCallBack && $.isFunction(self.closedItemCallBack) && self.closedItemCallBack(title, index, $(container).tabs('tabs').length);
         }
       });
-      $($tabs).tabs('add', {
+      $(container).tabs('add', {
         title: title,
         content: content,
         closable: true
       });
     }
+  },
+  bindEnv() {
+    var self = this;
+    var container = $(this.container);
+    $(this.tabItem).each(function(index, item){
+      $(item).click(function(){
+        var title = $(this).attr('data-title');
+        var value = $(this).attr('data-value');
+        var content = $('#tabContent'+value).html();
+        self.addTabs(title, content, container);
+      });
+    });
+  },
+  panelUpAndDown($this) {
+    // 面板收缩与展开
+    $this.find('.arrow_icon').unbind('click').bind('click', function(){
+      var $$this = $(this);
+      if (!$$this.hasClass('up')) {
+        $$this.addClass('up');
+        $this.find('.tabs-panels').slideUp('normal', function(){});
+      } else {
+        $$this.removeClass('up');
+        $this.find('.tabs-panels').slideDown('normal', function(){});
+      }
+    });
   }
 };
-AddTabs.prototype.panelUpAndDown = function($this) {
-  // 面板收缩与展开
-  $this.find('.arrow_icon').unbind('click').bind('click', function(){
-    var $$this = $(this);
-    if (!$$this.hasClass('up')) {
-      $$this.addClass('up');
-      $this.find('.tabs-panels').slideUp('normal', function(){});
-    } else {
-      $$this.removeClass('up');
-      $this.find('.tabs-panels').slideDown('normal', function(){});
-    }
-  });
-};
-AddTabs.prototype.setValue = function(value) {
-  if (value > 0) {
-    this.value = value;
-  }
-  this.addTabs();
-};
-AddTabs.prototype.getValue = function() {
-  return this.value;
-};
+window.AddTabs = AddTabs;
--- readme.md
+++ readme.md
@@ -4,27 +4,32 @@
     <div id="tabContent1" style="display: none;"><div>adsasddasdasadsadsads</div></div>
     <div id="tabContent2" style="display: none;"><div>啊啊啊啊所大多所多撒奥多所无</div></div>
     <div id="tabContent3" style="display: none;"><div>adsasdqweqweqwwrrdfggffggf</div></div>
-    <input text="number" id="inputId" style="width: 200px; height: 26px; border: 1px solid red;" />
-    <div id="containerId"></div>
+    <div style="margin-bottom:10px" id='testId'>
+      <a href="#" class="easyui-linkbutton J_Tabs" data-title='2015测试' data-value='1'>2015</a>
+      <a href="#" class="easyui-linkbutton J_Tabs" data-title='2016测试' data-value='2'>2016</a>
+      <a href="#" class="easyui-linkbutton J_Tabs" data-title='2017测试' data-value='3'>2017</a>
+    </div>
+    <div id="containerId" class="easyui-tabs hidden" style="width:400px;height:250px;"></div>
 #### javascript调用方式如下：
-    $("#inputId").keyup(function(e){
-      var value = e.target.value - 0;
-      if (value > 0) {
-       var $panel = new AddTabs({
-          value: value,
-          prefixContent: 'tabContent',
-          container: '#containerId',
-          closedItemCallBack: function(obj){
-            var len = obj.len;
-            var $this = obj.$this;
-            if (len === 0) {
-              $("#inputId").val('');
-            } else {
-              $("#inputId").val(len);
-            }
-            $this.setValue(len);
-          }
-        });
+#### 外部点击调用 
+    $("#testId").on('click', '.J_Tabs', function(){
+      var title = $(this).attr('data-title');
+      var value = $(this).attr('data-value');
+      var content = $('#tabContent'+value).html();
+      new AddTabs({
+        title: title,
+        content: content,
+        container: '#containerId'
+      });
+    });
+#### 内部调用 (已封装点击事件)
+    new AddTabs({
+      tabItem: '.J_Tabs',
+      container: '#containerId',
+      closedItemCallBack: function(title, index, len) {
+        console.log(title);
+        console.log(index);
+        console.log(len)
       }
     });
 <p>如上调用即可初始化。</p>
@@ -34,13 +39,14 @@
 |      属性      |             说明                               |     类型        |     默认值     |
 | --------------|:--------------------------------------------:  |   :-----------:| :-------------:|
 |   container   |  tab项的容器[必须的参数]                          | [String]      |  ''             |
-|   value       |  输入框的值[必须的参数]                            | [String]       |  ''            |
-|   prefixContent| 内容的前缀类名                                | [String]       |  ''            |
+|   title       |  tab某项的标题(可选)                             | [String]       |  ''            |
+|   content    |   tab项需要显示的内容(可选)                        | [String]       |  ''            |
+|   tabItem    |  tab的某一项传class                               | String         |  ''          |
 
 ####  回调方法
 |     方法名           |         说明         |     返回参数                                           | 
 | --------------------|:-------------------------:  |:--------------------------------------------: |
-| closedItemCallBack  |  关闭tan某一项的回调          | Object {$this, title, index, len}| 
+| closedItemCallBack  |  关闭tan某一项的回调          | title(被关闭标题), index(被关闭的索引), length(剩下多个个tab项标签) | 
 
 ### 页面查看效果 预览如下地址
-<p><a href="https://tugenhua0707.github.io/easyuiTab2/index.html" target="_blank">https://tugenhua0707.github.io/easyuiTab/index.html</a></p>
+<p><a href="https://tugenhua0707.github.io/easyuiTab/index.html" target="_blank">https://tugenhua0707.github.io/easyuiTab/index.html</a></p>
